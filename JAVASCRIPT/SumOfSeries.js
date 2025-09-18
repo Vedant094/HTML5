6bc69b8124b3bb79df60f9dccf4053e36bf0f784
Vedant094 <raturi.vedant003@gmail.com>
2025-09-18 12:26:26 +0530

    Add files via upload

---
+++ JAVASCRIPT/SumOfSeries.js
@@ -0,0 +1,8 @@
+var n,i=1,sum=0;
+n=parseInt(prompt("Enter a N Value: "));
+
+for(i=0;i<n;i++){
+    sum=sum+i;
+}
+document.write("<h2>The sum of series is: "+sum+"</h2");
+alert("The sum of series is: "+sum);
